b1a2603a0cda0c8ee488a6dc8a05fb9b1d753f52
Brian Han <brianhan87usa@gmail.com>
2019-08-26 17:35:42 -0500

    Move Layout.js into a Layout folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import styles from "./index.module.css"
+
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className={styles.root}>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div className={styles.image}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
